edc7d273f4ce2bc361b7ef93e3f91774ada56fc2
Brinda Shah <brinda.shah@bt.com>
2011-09-29 14:03:43 -0400

    update commit

--- HelloWord/src/com/bt/hello.java
+++ HelloWord/src/com/bt/hello.java
@@ -8,6 +8,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
+		System.out.println("testing update");
 	}
 
 }
